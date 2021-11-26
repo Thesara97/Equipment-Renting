4fd442fd571be9a5e6ff61b8db39a924b4e82bfc
Janith CB <42415329+janithcbk123@users.noreply.github.com>
2021-11-26 15:18:17 +0530

    Password Change

--- login-query.php
+++ login-query.php
@@ -7,7 +7,7 @@ if(isset($_POST['login'])){
     $username = $_POST['username'];
     $password = $_POST['password'];
 
-    if($username == "admin" && $password == "admin123"){
+    if($username == "admin" && $password == "Admin123"){
         $_SESSION['adminid'] = "1";
         header('Location: dashboard.php');
     }else{
