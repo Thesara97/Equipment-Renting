54b77124f422dc39530ef55bf0223d227b8877a2
janithcbk123 <janithcbk12janibandara123@gmail.com3>
2021-11-29 01:03:06 +0530

    Update Borrow equipment Query

---
+++ borrow-equipment-query.php
@@ -0,0 +1,22 @@
+<?php
+
+require_once('database/connection.php');
+
+$equipment        = $_POST['equipment'];
+$user      = $_POST['user'];
+$return    = $_POST['returndate'];
+
+$today = date('Y-m-d');
+
+$insert_bequipment = "INSERT INTO `borrowequipment`(`equipmentid`, `userid`, `issuedate`, `returndate`) VALUES ('$equipment','$user','$today','$return')";
+
+if (mysqli_query($con, $insert_bequipment)) {
+    // echo "New record created successfully";
+    $last_id = mysqli_insert_id($con);
+    header('Location: borrow-cd-finish.php?borrowid='.$last_id);
+
+} else {
+    echo "Error: " . $insert_bequipment . "<br>" . mysqli_error($con);
+}
+
+?>
