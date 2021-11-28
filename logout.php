fa6f0a13c9ecd35e8c997a7dc78f31291a9ad27d
janithcbk123 <janithcbk12janibandara123@gmail.com3>
2021-11-28 19:24:16 +0530

    Create Logout

---
+++ logout.php
@@ -0,0 +1,14 @@
+<?php 
+	
+	session_start();
+
+	$_SESSION = array();
+
+	if (isset($_COOKIE[session_name()])) {
+		setcookie(session_name(), '' , time()-86400, '/');
+	}
+
+	session_destroy();
+
+	header('location: index.php');
+ ?>
