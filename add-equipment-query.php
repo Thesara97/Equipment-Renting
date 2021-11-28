582e5e8071dffaa539b642f5130941af48a0253c
janithcbk123 <janithcbk12janibandara123@gmail.com3>
2021-11-29 00:51:38 +0530

    Update Equipment Query

--- add-equipment-query.php
+++ add-equipment-query.php
@@ -2,12 +2,12 @@
 
 require_once('database/connection.php');
 
-$title       = $_POST['equipment'];
-$author     = $_POST['manufacturer'];
+$equipment       = $_POST['equipment'];
+$manufacturer     = $_POST['manufacturer'];
 $fee        = $_POST['fee'];
 $category    = $_POST['category'];
 
-$insert_equipment = "INSERT INTO `equipment`(`equipment`, `manufacturer`, `price`, `category`, `status`) VALUES ('$title','$manufacturer','$fee','$category','Available')";
+$insert_equipment = "INSERT INTO `equipment`(`equipment`, `manufacturer`, `price`, `category`, `status`) VALUES ('$equipment','$manufacturer','$fee','$category','Available')";
 if (mysqli_query($con, $insert_equipment)) {
     // echo "New record created successfully";
     header('Location: add-equipment.php');
