a817061bb9aa5b3b3035e9a532790df5a06959bc
janithcbk123 <janithcbk12janibandara123@gmail.com3>
2021-12-05 14:26:31 +0530

    UI Update

--- index-incorrect.php
+++ index-incorrect.php
@@ -33,9 +33,7 @@
                             <div class="form-group">
                                 <label for="password">Password</label>
                                 <input type="password" class="form-control" id="password" name="password" placeholder="Password" required>
-                            </div>
-                            <div class="form-group">
-                                <label for="password"><p style="color:red"> Incorrect Username or Password <p> </label>
+                                <small><p style="color:red">Incorrect Username or Password</small>
                             </div>
                            
                             <button type="submit" class="btn btn-primary" name="login">Login</button>
