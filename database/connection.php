127a798bf0dec56efe1afb84b6e0b55f808573a3
janithcbk123 <janithcbk12janibandara123@gmail.com3>
2021-12-13 12:14:47 +0530

    Convert to OOP

--- database/connection.php
+++ database/connection.php
@@ -1,9 +1,47 @@
 <?php
-$con = mysqli_connect("localhost","root","","equipment");
-
+//connection.php
+$con = new mysqli("localhost", "root", "", "equipment");
+ 
 // Check connection
-if (mysqli_connect_errno()) {
-  echo "Failed to connect to MySQL: " . mysqli_connect_error();
-  exit();
+if($con === false){
+    die("ERROR: Could not connect. " . $mysqli->connect_error);
 }
+
+class Databases{  
+  public $con;  
+  public function __construct()  
+  {  
+       $this->con = mysqli_connect("localhost", "root", "", "equipment");  
+       if(!$this->con)  
+       {  
+            echo 'Database Connection Error ' . mysqli_connect_error($this->con);  
+       }  
+  }  
+  public function insert($table_name, $data)  
+  {  
+       $string = "INSERT INTO ".$table_name." (";            
+       $string .= implode(",", array_keys($data)) . ') VALUES (';            
+       $string .= "'" . implode("','", array_values($data)) . "')";  
+       if(mysqli_query($this->con, $string))  
+       {  
+            return true;  
+       }  
+       else  
+       {  
+            echo mysqli_error($this->con);  
+       }  
+  }  
+  public function select($table_name)  
+  {  
+       $array = array();  
+       $query = "SELECT * FROM ".$table_name."";  
+       $result = mysqli_query($this->con, $query);  
+       while($row = mysqli_fetch_assoc($result))  
+       {  
+            $array[] = $row;  
+       }  
+       return $array;  
+  }  
+}  
+ 
 ?>
