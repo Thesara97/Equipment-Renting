ce8ea4d63bcf4cecf66a3fd497098d6f909d9cb6
janithcbk123 <janithcbk12janibandara123@gmail.com3>
2021-12-19 12:34:06 +0530

    Convert to OOP

--- add-equipment-query.php
+++ add-equipment-query.php
@@ -1,6 +1,7 @@
 <?php
 
-require_once('database/connection.php');
+include('database/connection.php');
+$data = new Databases;
 
 $equipment       = $_POST['equipment'];
 $manufacturer     = $_POST['manufacturer'];
@@ -8,12 +9,10 @@ $fee        = $_POST['fee'];
 $category    = $_POST['category'];
 
 $insert_equipment = "INSERT INTO `equipment`(`equipment`, `manufacturer`, `price`, `category`, `status`) VALUES ('$equipment','$manufacturer','$fee','$category','Available')";
-if (mysqli_query($con, $insert_equipment)) {
-    // echo "New record created successfully";
+if($data->con->query($insert_equipment) === true){
+    //echo "New record created successfully";
     header('Location: add-equipment.php');
-
-} else {
-    echo "Error: " . $insert_equipment . "<br>" . mysqli_error($con);
+} else{
+    echo "ERROR: Could not able to execute $insert_equipment. " . $$data->con->error;
 }
-
 ?>
