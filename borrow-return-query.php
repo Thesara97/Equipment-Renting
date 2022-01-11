6d82d0f16e70863b4ce222a3b31500237308d576
Thesara97 <thesara881@gmail.com>
2022-01-12 00:07:39 +0530

    bug fix

--- borrow-return-query.php
+++ borrow-return-query.php
@@ -1,30 +1,45 @@
 <?php
 
-require_once('database/connection.php');
+include('database/connection.php');
+$data = new Databases;
 
 $borrowid    = $_GET['id'];
 
 
-$select_borrow_data = "SELECT * FROM `borrowequipment` WHERE id = '$borrowid'";
-$select_borrow_data_result = mysqli_query($con, $select_borrow_data);
+$select_borrow_data = "SELECT * FROM borrowequipment WHERE id = '$borrowid'";
+$select_borrow_data_result = mysqli_query($data->con, $select_borrow_data);
 $row_borrow_data = mysqli_fetch_assoc($select_borrow_data_result);
 
 $equipmentid = $row_borrow_data['equipmentid'];
 $userid = $row_borrow_data['userid'];
 
-$update_bequipment = "UPDATE `borrowequipment` SET `status` = 'finish' WHERE `id` = '$borrowid'";
+$update_bequipment = "UPDATE borrowequipment SET status = 'finish' WHERE id = '$borrowid'";
 
-$update_equipment = "UPDATE `equipment` SET `status` = 'available' WHERE `id` = '$equipmentid'";
-$update_result = mysqli_query($con, $update_equipment);
+$update_equipment = "UPDATE equipment SET status = 'available' WHERE id = '$equipmentid'";
+$update_result = mysqli_query($data->con, $update_equipment);
 
 
-if (mysqli_query($con, $update_bequipment)) {
-    // echo "New record created successfully";
-  
-    header('Location: borrow-history.php');
+if($data->con->query($update_bequipment) === true){
 
-} else {
-    echo "Error: " . $update_bequipment . "<br>" . mysqli_error($con);
+
+        $get_panelty = "SELECT * FROM settings WHERE id = '1'";
+        $result_getpanelty = mysqli_query($data->con, $get_panelty);
+        $row_gellpanelty = mysqli_fetch_assoc($result_getpanelty);
+
+        $newnewfee = ($row_gellpanelty['currentpanelty'] + $row_gellpanelty['totalpanelty']);
+
+        $update_fee = "UPDATE settings SET totalpanelty ='$newnewfee' , currentpanelty ='0' WHERE id = 1";
+
+        if($data->con->query($update_fee) === true){
+            //echo "New record created successfully";
+            header('Location: borrow-history.php');
+        } else{
+            echo "ERROR: Could not able to execute $update_fee. " . $data->con->error;
+
+        }
+
+} else{
+    echo "ERROR: Could not able to execute $update_bequipment. " . $data->con->error;
 }
 
 ?>
